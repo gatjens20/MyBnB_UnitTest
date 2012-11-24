620f04d0f41dedbc36827a09e2923784dffe1c90
Otto Gatjens Sancho <gatjens20@hotmail.com>
2012-11-24 04:55:16 -0600

    Otto Gatjens Sancho
    PUTAAAAAAAAAAAAA

---
+++ ControllerComentario_UnitTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using MyBnb.Controller;
+
+namespace MyBnBTestFixture
+{    
+    public class DerivedControllerComentario : ControllerComentario
+    {
+        //public DerivedControllerComentario() : base() { }
+
+        //public void realizarComentario_Derived(int pindiceRanking, string pcomentario)
+        //{
+        //    //base.realizarComentario(pindiceRanking, pcomentario);
+        //}
+
+    }
+
+    [TestFixture]
+    public class ControllerComentario_UnitTest
+    {
+        //#region Variables
+        //ControllerComentario controllerComentarioObj;
+        //DerivedControllerComentario DerivedControllerComentarioObj;
+        //int pindiceRanking; 
+        //String pcomentario;
+        //#endregion
+
+        //[TestFixtureSetUp]
+        //public void Init()
+        //{
+        //    controllerComentarioObj = new ControllerComentario();
+        //    DerivedControllerComentarioObj = new DerivedControllerComentario();
+        //    pindiceRanking = 1;
+        //    pcomentario = "Nuevo Comentario sobre una propiedad ya visitada";
+        //}
+
+        //[Test]
+        //public void RealizarComentario_Test()
+        //{
+        //    DerivedControllerComentarioObj.realizarComentario_Derived(pindiceRanking, pcomentario);
+        //}
+
+        //[Test]
+        //public void getLogin_Test()
+        //{
+        //    controllerComentarioObj.
+        //}
+
+
+    }
+}
---
+++ ControllerUsuario_UnitTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using MyBnb.Controller;
+
+namespace MyBnBTestFixture
+{       
+    [TestFixture]
+    public class ControllerUsuario_UnitTest
+    {
+        #region Variables
+        ControllerUsuario ControllerUsuarioObj;
+        #endregion
+
+        [TestFixtureSetUp]
+        public void Init()
+        {
+            ControllerUsuarioObj = new ControllerUsuario();
+        }
+
+        [Test]
+        public void getLogin_Test()
+        {
+            ControllerUsuarioObj.getLogin();
+        }
+
+
+    }
+}
